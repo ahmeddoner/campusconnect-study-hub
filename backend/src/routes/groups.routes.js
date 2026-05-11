e5705d3193ee9bb8b7841c8f19a2c2242739dfd6
abdulrehman <instagramabdulrehman03gmail.com>
2026-05-11 14:47:31 +0200

    Add inline comments to groups routes

--- backend/src/routes/groups.routes.js
+++ backend/src/routes/groups.routes.js
@@ -1,7 +1,7 @@
 const express = require("express");
-const router = express.Router();
-const requireAuth = require("../middleware/auth.middleware");
-const {
+const router = express.Router();      // mini router onyl for group routes
+const requireAuth = require("../middleware/auth.middleware");   // checks Jwt
+const {              // This imports functions from controller
   getGroups,
   createGroup,
   getGroup,
@@ -19,7 +19,7 @@ router.post("/", createGroup);
 router.get("/:id", getGroup);
 router.put("/:id", updateGroup);
 router.delete("/:id", deleteGroup);
-router.post("/join", joinGroup);
+router.post("/join", joinGroup);    // post bcs request is sending data (join code) in request body
 router.delete("/:id/leave", leaveGroup);
 router.delete("/:id/members/:userId", removeMember);
 
